40253e9b8e2bbc6e48dda0ce1e2f69bd5db9c984
Ti <nguyenvantoan18062006@gmail.com>
2025-03-14 07:00:22 +0700

    Bước 2.2
    Xây dựng xe tăng người chơi

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int TILE_SIZE = 40;
-const int MAP_WIDTH = SCREEN_WIDTH / TILE_SIZE;
-const int MAP_HEIGHT = SCREEN_HEIGHT/ TILE_SIZE;
+const int MAP_WIDTH =  SCREEN_WIDTH / TILE_SIZE;
+const int MAP_HEIGHT =  SCREEN_HEIGHT/ TILE_SIZE;
 
 class Wall {
 public:
@@ -20,6 +20,50 @@ public:
     void render(SDL_Renderer* renderer);
 };
 
+class PlayerTank{
+public:
+    int x, y;
+    int dirX, dirY;
+    SDL_Rect rect;
+
+    PlayerTank(int startX, int startY) {
+        x = startX;
+        y = startY;
+        rect = {x, y, TILE_SIZE, TILE_SIZE};
+        dirX = 0;
+        dirY = -1;                                              // mac dinh tank di len (truc y huong xuong)
+    }
+
+    void move(int dx, int dy, const vector<Wall>& walls);
+
+    void render(SDL_Renderer* renderer);
+};
+
+void PlayerTank::render(SDL_Renderer* renderer){
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+void PlayerTank::move(int dx, int dy, const vector<Wall>& walls) {
+    int newX = x+dx;
+    int newY = y+dy;
+    this->dirX = dx;
+    this->dirY = dy;
+
+    SDL_Rect newRect = {newX, newY, TILE_SIZE, TILE_SIZE};
+    for(int i = 0; i<walls.size(); i++) {
+        if(walls[i].active && SDL_HasIntersection(&newRect, &walls[i].rect))            //kiem tra su va cham voi tuong
+            return;
+    }
+
+    if(newX >= TILE_SIZE && newX <= SCREEN_WIDTH - TILE_SIZE*2 &&
+       newY >= TILE_SIZE && newY <= SCREEN_HEIGHT - TILE_SIZE*2)  {
+        x = newX;
+        y = newY;
+        rect.x = x;
+        rect.y = y;
+       }
+}
 
 class Game{
 public:
@@ -27,11 +71,12 @@ public:
     SDL_Renderer* renderer;
     bool running;
     vector<Wall> walls;
-
+    PlayerTank player{((MAP_WIDTH -1)/2)*TILE_SIZE, (MAP_HEIGHT - 2)*TILE_SIZE};
 
     Game();
     void generateWalls();
     void render(SDL_Renderer* renderer);
+    void handleEvents();
     void run();
     ~Game();
 };
@@ -71,12 +116,31 @@ void Game::render(SDL_Renderer* renderer) {
     for(int i = 0; i<(int)walls.size(); i++) {
         walls[i].render(renderer);
     }
+    player.render(renderer);                    // ve xe tang cua nguoi choi
 
     SDL_RenderPresent(renderer);                                               // hien thi len man hinh
 }
 
+void Game::handleEvents() {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        if(event.type == SDL_QUIT) {
+            running =false;
+        }   else if(event.type = SDL_KEYDOWN) {
+            switch(event.key.keysym.sym) {
+                case SDLK_UP: player.move(0, -5, walls); break;
+                case SDLK_DOWN: player.move(0, 5, walls); break;
+                case SDLK_LEFT: player.move(-5, 0, walls); break;
+                case SDLK_RIGHT: player.move(5, 0, walls); break;
+            }
+
+        }
+    }
+}
+
 void Game::run() {
     while(running) {
+        handleEvents();
         render(renderer);
         SDL_Delay(16);
     }
@@ -98,7 +162,7 @@ void Wall::render( SDL_Renderer* renderer){
 }
 
 void Game::generateWalls() {
-    for(int i = 3; i<MAP_HEIGHT; i+= 2) {
+    for(int i = 3; i<MAP_HEIGHT - 2; i+= 2) {
         for(int j = 3; j< MAP_WIDTH-3; j+=2) {
             Wall w = Wall(j*TILE_SIZE, i*TILE_SIZE);
             walls.push_back(w);
